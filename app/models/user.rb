65c4c6acdf4726e4eff3284989f9403fd366bcf8
vu.quoc.bao <vu.quoc.bao@sun-asterisk.com>
2023-06-22 10:19:58 +0700

    Login-Logout

---
+++ app/models/user.rb
@@ -0,0 +1,50 @@
+class User < ApplicationRecord
+  attr_accessor :remember_token
+
+  enum role: {guest: 0, admin: 1, user: 2}
+
+  before_save :downcase_email
+
+  validates :name, presence: true, length: {maximum: Settings.max_length_name_20}
+  validates :email, presence: true, length: {minimum: Settings.min_length_email_20,
+                                             maximum: Settings.max_length_email_255},
+    format: {with: Regexp.new(Settings.valid_email_regex)}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: Settings.min_length_password_6}, allow_nil: true
+
+  has_secure_password
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  def authenticated? remember_token
+    return false unless remember_token
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+end
---
+++ db/migrate/20230626015515_create.rb
@@ -0,0 +1,16 @@
+class Create < ActiveRecord::Migration[6.1]
+  def change
+    drop_table :users
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :password_digest
+      t.string :remember_digest
+      t.integer :role, default: 0
+      t.index ["email"], name: "index_users_on_email", unique: true
+
+      t.timestamps
+    end
+  end
+end
