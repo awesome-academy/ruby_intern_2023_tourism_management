396a43fd16002a9102e11be43e5c91aa22386225
vu.quoc.bao <vu.quoc.bao@sun-asterisk.com>
2023-07-03 15:00:23 +0700

    Order history

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
   include SessionsHelper
-  include Pagy::Backend
   before_action :set_locale
 
   protect_from_forgery with: :exception
@@ -12,5 +11,20 @@ class ApplicationController < ActionController::Base
 
   def default_url_options
     {locale: I18n.locale}
+  end
+
+  def find_order_by_id
+    @order = Order.find_by id: params[:id]
+    return if @order
+
+    flash[:danger] = t "orders.flash.cant_find_order"
+    redirect_to current_user.admin? ? admin_orders_path : (user_orders_path current_user)
+  end
+
+  def check_status_pending
+    return if @order.pending?
+
+    flash[:danger] = t "admin.orders.flash.order_status_changed"
+    redirect_to current_user.admin? ? admin_orders_path : (user_orders_path current_user)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,8 @@ class Order < ApplicationRecord
   belongs_to :user
 
   delegate :name, :phone, :email, :address, to: :user, prefix: true, allow_nil: true
-  delegate :name, :image, :cost, :start_date, :end_date, :tour_guide_cost, to: :tour, prefix: true, allow_nil: true
+  delegate :name, :image, :cost, :start_date, :end_date, :tour_guide_cost, :visit_location, to: :tour,
+           prefix: true, allow_nil: true
 
   scope :newest, ->{order updated_at: :desc}
   scope :filter_by_status, ->(status){where(status: status) if status.present? && status != Settings.all_status_value}
@@ -13,6 +14,7 @@ class Order < ApplicationRecord
 
   enum status: {pending: 0, approved: 1, cancelled: 2}
   validates :amount_member, :contact_name, :contact_phone, :contact_address, presence: true
+  validates :amount_member, numericality: {only_integer: true, greater_than: 0}
 
   def display_image
     tour_image.variant resize_to_limit: Array.new(Settings.limit_image_area)
